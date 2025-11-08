57d7878da26bbf811e1ac9b04c591218076f0f50
mac <mac@pop-os>
2025-11-07 21:03:52 -0600

    Second

---
+++ src/main/java/com/mycompany/missiontracker/MissionTracker.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.missiontracker;
+
+/**
+ *
+ * @author mac
+ */
+public class MissionTracker {
+
+    public static void main(String[] args) {
+        System.out.println("Helo World!");
+    }
+}
